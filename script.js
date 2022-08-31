0860c60dbf6830bb6f62be1774e330584075ac96
Danypooh <delatorrechapell@gmail.com>
2022-08-31 10:41:34 -0500

    primera version de calculo de triangulo

---
+++ script.js
@@ -0,0 +1,18 @@
+let base;
+let altura;
+let area;
+
+function calcularArea() {
+  let baseElement = document.getElementById("base");
+  let alturaElement = document.getElementById("altura");
+  base = baseElement.value;
+  altura = alturaElement.value;
+  console.log(base);
+  console.log(altura);
+
+  area = (base * altura) / 2;
+  console.log(area);
+
+  let areaElement = document.getElementById("resultado");
+  areaElement.innerHTML = 'Area = ' + area;
+}
